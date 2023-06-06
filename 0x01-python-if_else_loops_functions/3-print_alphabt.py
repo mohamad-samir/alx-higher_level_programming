39a9dafb777755eee6cddf923a2561f8309c86b3
mohamad-samir <mohamad.samir2011@gmail.com>
2023-06-06 20:47:20 +0300

    update

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,19 +1,5 @@
-#include <stdio.h>
-
-/**
- * main - prints the alphabet in lowercase, and then in uppercase,
- * followed by a new line
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int ch;
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-		putchar(ch);
-	for (ch = 'A'; ch <= 'Z'; ch++)
-		putchar(ch);
-	putchar('\n');
-	return (0);
-}
-
+#!/usr/bin/python3
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{:c}".format(i), end='')
+        
