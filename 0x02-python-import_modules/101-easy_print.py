aaf9b853501340b585476d0fb9a05a9286fd263c
mohamad-samir <mohamad.samir2011@gmail.com>
2023-06-17 17:39:24 +0300

    update

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-import builtins; builtins.__dict__.get("__import__")("os").write(1, b"#pythoniscool\n")
+import builtins.__dict__.get("__import__")("os").write(1, b"#pythoniscool\n")
