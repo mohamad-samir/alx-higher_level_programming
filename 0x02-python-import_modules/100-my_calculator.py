0b716c72844cf18ce7c3c9134ee7e9e5d3a5109a
mohamad-samir <mohamad.samir2011@gmail.com>
2023-06-17 17:11:16 +0300

    update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
 # 100-my_calculator.py
 
-if __name__ == "__main__":
-	"""Handle basic arithmetic operations."""
-	import sys
-	from calculator_1 import add, sub, mul, div
+"""Handle basic arithmetic operations."""
 
-	if len(sys.argv) != 4:
-		print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-		sys.exit(1)
+from calculator_1 import add, sub, mul, div
+import sys
 
-	operations = {"+": add, "-": sub, "*": mul, "/": div}
+"""Check if the number of arguments is correct."""
+if len(sys.argv) - 1 != 3:
+	print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+	sys.exit(1)
 
-	operator = sys.argv[2]
-	if operator not in operations:
-		print("Unknown operator. Available operators: +, -, * and /")
-		sys.exit(1)
+"""Create a dictionary of operators and their corresponding functions."""
+ops = {"+": add, "-": sub, "*": mul, "/": div}
 
-	a = int(sys.argv[1])
-	b = int(sys.argv[3])
+"""Check if the operator is valid."""
+if sys.argv[2] not in list(ops.keys()):
+	print("Unknown operator. Available operators: +, -, * and /")
+	sys.exit(1)
 
-	result = operations[operator](a, b)
-	print(f"{a} {operator} {b} = {result}")
+"""Get the two numbers and the operator."""
+a = int(sys.argv[1])
+b = int(sys.argv[3])
+
+"""Perform the arithmetic operation and print the result."""
+print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
