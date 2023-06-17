c4aca28dce48243f4cc70c870d7494bb73c3735a
mohamad-samir <mohamad.samir2011@gmail.com>
2023-06-17 17:18:05 +0300

    update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
+# 100-my_calculator.py
 
-import sys
-import calculator_1
+if __name__ == "__main__":
+    """Handle basic arithmetic operations."""
+    import sys
+    from calculator_1 import add, sub, mul, div
 
-"""Handle basic operations."""
-if len(sys.argv) != 3:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
+    # التحقق من عدد الوسائط المدخلة
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-a = int(sys.argv[1])
-operator = sys.argv[2]
-b = int(sys.argv[3])
+    # قاموس يعين كل عملية لوظيفتها المقابلة
+    operations = {"+": add, "-": sub, "*": mul, "/": div}
 
-try:
-    func = getattr(calculator_1, operator)
-    result = func(a, b)
+    # التحقق مما إذا كان المشغل المدخل صحيحًا
+    operator = sys.argv[2]
+    if operator not in operations:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    # تحويل الأعداد إلى أرقام صحيحة
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    # تنفيذ العملية المحددة وطباعة النتيجة
+    result = operations[operator](a, b)
     print(f"{a} {operator} {b} = {result}")
-except AttributeError:
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
