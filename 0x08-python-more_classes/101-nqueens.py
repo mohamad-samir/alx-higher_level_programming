af465684cd1012b33ee338e491c86f47bcf1bc00
mohamad-samir <mohamad.samir2011@gmail.com>
2023-07-04 22:09:03 +0300

    update

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,74 +1,70 @@
-#!/usr/bin/python3
-"""A program that solves the N queens problem."""
+#!/usr/bin/env python3
+"""
+Solve the N queens problem.
+
+Usage: nqueens N
+where N must be an integer greater or equal to 4
+"""
+
 import sys
 
-def solve_nqueens(N):
-    def is_safe(board, row, col):
-        # Check if there is a queen in the same column
-        for i in range(row):
-            if board[i][col] == 1:
-                return False
 
-        # Check if there is a queen in the upper left diagonal
-        i = row
-        j = col
-        while i >= 0 and j >= 0:
-            if board[i][j] == 1:
-                return False
-            i -= 1
-            j -= 1
+def print_board(board):
+    """
+    Print the given board in a human-readable way.
+    """
+    for row in board:
+        print(' '.join(map(str, row)))
 
-        # Check if there is a queen in the upper right diagonal
-        i = row
-        j = col
-        while i >= 0 and j < N:
-            if board[i][j] == 1:
-                return False
-            i -= 1
-            j += 1
 
-        return True
+def is_valid(board, row, col):
+    """
+    Return True if the given row and column are valid for a queen.
+    """
+    for i in range(row):
+        if board[i][col] == 'Q':
+            return False
+        if abs(row - i) == abs(col - board[i][col]):
+            return False
+    return True
 
-    def backtrack(board, row):
-        if row == N:
-            # Found a solution, add it to the list of solutions
-            solutions.append([list(row) for row in board])
-            return True
 
-        for col in range(N):
-            if is_safe(board, row, col):
-                board[row][col] = 1
-                backtrack(board, row + 1)
-                board[row][col] = 0
+def solve(board, row):
+    """
+    Solve the N queens problem recursively.
+    """
+    if row == len(board):
+        print_board(board)
+        return
 
-    # Initialize the board
-    board = [[0] * N for _ in range(N)]
-    solutions = []
+    for col in range(len(board)):
+        if is_valid(board, row, col):
+            board[row][col] = 'Q'
+            solve(board, row + 1)
+            board[row][col] = '.'
 
-    # Start the backtracking algorithm
-    backtrack(board, 0)
 
-    return solutions
-
-if __name__ == "__main__":
-    # Validate the input
+def main():
+    """
+    Parse the command-line arguments and solve the N queens problem.
+    """
     if len(sys.argv) != 2:
-        print("Usage: nqueens N")
+        print('Usage: nqueens N')
         sys.exit(1)
 
     try:
         N = int(sys.argv[1])
     except ValueError:
-        print("N must be a number")
+        print('N must be a number')
         sys.exit(1)
 
     if N < 4:
-        print("N must be at least 4")
+        print('N must be at least 4')
         sys.exit(1)
 
-    # Solve the N Queens problem
-    solutions = solve_nqueens(N)
+    board = [['.' for _ in range(N)] for _ in range(N)]
+    solve(board, 0)
 
-    # Print the solutions
-    for solution in solutions:
-        print(solution)
+
+if __name__ == '__main__':
+    main()
