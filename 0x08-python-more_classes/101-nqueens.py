232da697865b980082ac905ebdde753a258583eb
mohamad-samir <mohamad.samir2011@gmail.com>
2023-07-04 22:19:21 +0300

    update

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -19,7 +19,7 @@ class Solution_Board:
 
     @num.setter
     def num(self, value):
-        if not isinstance(num, int):
+        if not isinstance(value, int):
             raise TypeError("num should be an int")
         self.__num = value
 
@@ -37,20 +37,53 @@ if num < 4:
     exit(1)
 
 solutions = []
-board = [[0 for a in range(0, num)] for b in range(0, num)]
-running = True
-while running:
-    sol = get_n_queens(board)
-    solutions.append(sol)
-    running = False
+board = [[0 for _ in range(0, num)] for _ in range(0, num)]
 
-def get_n_queens(chess_board, column, num):
+def get_n_queens(chess_board, column=0, num=0):
     if column >= num:
         return True
     for i in range(0, num):
-        if board_safe(chess_board, column):
+        if board_safe(chess_board, i, column):
             chess_board[i][column] = 1
-            if get_n_queens(chess_board, column + 1):
+            if get_n_queens(chess_board, column + 1, num):
                 return True
-            board[i][column] = 0
+            chess_board[i][column] = 0
     return False
+
+def board_safe(chess_board, row, column):
+    # Check if there is a queen in the same column
+    for i in range(row):
+        if chess_board[i][column] == 1:
+            return False
+
+    # Check if there is a queen in the upper left diagonal
+    i = row
+    j = column
+    while i >= 0 and j >= 0:
+        if chess_board[i][j] == 1:
+            return False
+        i -= 1
+        j -= 1
+
+    # Check if there is a queen in the upper right diagonal
+    i = row
+    j = column
+    while i >= 0 and j < num:
+        if chess_board[i][j] == 1:
+            return False
+        i -= 1
+        j += 1
+
+    return True
+
+running = True
+while running:
+    sol = get_n_queens(board, column=0, num=num)
+    if sol:
+        solutions.append([list(row) for row in board])
+    else:
+        running = False
+
+# Print the solutions
+for solution in solutions:
+    print(solution)
