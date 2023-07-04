31fb37356443d93e3fbd8febb73db3e369884f51
mohamad-samir <mohamad.samir2011@gmail.com>
2023-07-04 22:11:08 +0300

    update

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -42,29 +42,3 @@ def solve(board, row):
             board[row][col] = 'Q'
             solve(board, row + 1)
             board[row][col] = '.'
-
-
-def main():
-    """
-    Parse the command-line arguments and solve the N queens problem.
-    """
-    if len(sys.argv) != 2:
-        print('Usage: nqueens N')
-        sys.exit(1)
-
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
-        print('N must be a number')
-        sys.exit(1)
-
-    if N < 4:
-        print('N must be at least 4')
-        sys.exit(1)
-
-    board = [['.' for _ in range(N)] for _ in range(N)]
-    solve(board, 0)
-
-
-if __name__ == '__main__':
-    main()
