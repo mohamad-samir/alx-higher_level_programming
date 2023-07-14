d6a52d81e60c853465c3e3049dcbb808ba73e0c3
mohamad-samir <mohamad.samir2011@gmail.com>
2023-07-14 17:04:44 +0300

    update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,15 +7,23 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
+
 try:
     # Load the existing list from the file, if it exists
     my_list = load_from_json_file(filename)
 except FileNotFoundError:
     # If the file doesn't exist, create an empty list
     my_list = []
+except json.JSONDecodeError:
+    print("Invalid JSON format in the file.")
+    sys.exit(1)
 
 # Add the arguments to the list
 my_list.extend(sys.argv[1:])
 
 # Save the updated list to the file
-save_to_json_file(my_list, filename)
+try:
+    save_to_json_file(my_list, filename)
+except Exception as e:
+    print(f"Error saving the list to the file: {e}")
+    sys.exit(1)
