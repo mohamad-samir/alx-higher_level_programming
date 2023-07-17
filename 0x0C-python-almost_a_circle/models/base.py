966d234373dc9daed479a7ab7b02cf4b5f677f78
mohamad-samir <mohamad.samir2011@gmail.com>
2023-07-17 22:02:48 +0300

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    Base Module
+	Base Module
 """
 
 import csv
@@ -21,36 +21,12 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-    @staticmethod
-    def to_json_string(list_dictionaries) -> str:
-        """Serializes a list of dictionaries to JSON string.
-
-        Args:
-            list_dictionaries (list of dicts): List of dictionaries.
-
-        Returns:
-            str: JSON string representation of the list of dictionaries.
-        """
-        return json.dumps(list_dictionaries) if list_dictionaries else "[]"
-
-    @staticmethod
-    def from_json_string(json_string) -> any:
-        """Deserializes a JSON string to Python object.
-
-        Args:
-            json_string (JSON str): The JSON string to deserialize.
-
-        Returns:
-            any: A Python object.
-        """
-        return json.loads(json_string) if json_string else []
-
     @classmethod
     def save_to_file(cls, list_objs):
         """Writes the JSON string representation of `list_objs` to a file.
 
         Args:
-            list_objs (list of objs): List of objects to be written.
+                list_objs (list of objs): List of objects to be written.
         """
         filename = f"{cls.__name__}.json"
         with open(filename, mode="w", encoding="utf-8") as json_file:
@@ -65,10 +41,10 @@ class Base:
         """Creates an instance with all attributes set.
 
         Args:
-            dictionary: Dictionary of arguments to be used to create instance.
+                dictionary: Dictionary of arguments to be used to create instance.
 
         Returns:
-            object: An instance of a class that inherits from this class.
+                object: An instance of a class that inherits from this class.
         """
 
         if cls.__name__ == "Rectangle":
@@ -85,7 +61,7 @@ class Base:
         """Reads data from a JSON file and creates instances with the data.
 
         Returns:
-            list: A list of instances created using the read data.
+                list: A list of instances created using the read data.
         """
         filename = f"{cls.__name__}.json"
         if not Path(filename).is_file():
@@ -101,7 +77,7 @@ class Base:
         """Serializes a list of objects to a CSV file.
 
         Args:
-            list_objs (list): List of objects whose data is to be saved to CSV.
+                list_objs (list): List of objects whose data is to be saved to CSV.
         """
         filename = f"{cls.__name__}.csv"
         with open(filename, mode="w", encoding="utf-8") as csv_file:
@@ -118,7 +94,7 @@ class Base:
         """Deserializes a CSV file and uses the data to create new objects.
 
         Returns:
-            list: List of objects created with the data read from the CSV.
+                list: List of objects created with the data read from the CSV.
         """
         filename = f"{cls.__name__}.csv"
 
@@ -137,6 +113,30 @@ class Base:
                 for row in reader
             ]
             return [cls.create(**(args)) for args in reader]
+
+    @staticmethod
+    def to_json_string(list_dictionaries) -> str:
+        """Serializes a list of dictionaries to JSON string.
+
+        Args:
+                list_dictionaries (list of dicts): List of dictionaries.
+
+        Returns:
+                str: JSON string representation of the list of dictionaries.
+        """
+        return json.dumps(list_dictionaries) if list_dictionaries else "[]"
+
+    @staticmethod
+    def from_json_string(json_string) -> any:
+        """Deserializes a JSON string to Python object.
+
+        Args:
+                json_string (JSON str): The JSON string to deserialize.
+
+        Returns:
+                any: A Python object.
+        """
+        return json.loads(json_string) if json_string else []
 
     @staticmethod
     def draw(list_rectangles, list_squares):
