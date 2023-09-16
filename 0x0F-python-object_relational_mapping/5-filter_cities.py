a2f35128ad802dd40018890e168d5f470316679d
mohamad-samir <mohamad.samir2011@gmail.com>
2023-09-16 22:20:03 +0300

    u

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Lists all cities of a state from the database hbtn_0e_4_usa
-using a state name provided as an argument
+Lists all cities from the database hbtn_0e_4_usa
 """
 import sys
 import MySQLdb
@@ -10,17 +9,13 @@ if __name__ == "__main__":
     username: str = sys.argv[1]
     password: str = sys.argv[2]
     db_name: str = sys.argv[3]
-    arg: str = sys.argv[4]
     host: str = "localhost"
     port: int = 3306
 
     statement: str = """
-    SELECT c.id, c.name, s.name
-    FROM cities AS c
-    JOIN states AS s
-    ON c.state_id = s.id
-    HAVING BINARY s.name = %s
-    ORDER BY c.id;
+    SELECT *
+    FROM cities
+    ORDER BY id;
     """
 
     db = MySQLdb.connect(
@@ -32,8 +27,7 @@ if __name__ == "__main__":
     )
     cursor = db.cursor()
 
-    cursor.execute(statement, (arg,))
+    cursor.execute(statement)
     rows = cursor.fetchall()
-    cities = tuple(row[1] for row in rows)
-    cities = ', '.join(cities)
-    print(cities)
+    for row in rows:
+        print(row)
