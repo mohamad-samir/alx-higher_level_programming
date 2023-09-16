083a346aad38749497679418ccdf1f6b5254cd28
mohamad-samir <mohamad.samir2011@gmail.com>
2023-09-16 22:23:36 +0300

    u

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Lists all values in the states table of hbtn_0e_0_usa where name matches the argument (safe from MySQL injection)
+Lists all cities from the database hbtn_0e_4_usa
 """
 import sys
 import MySQLdb
@@ -9,7 +9,6 @@ if __name__ == "__main__":
     username: str = sys.argv[1]
     password: str = sys.argv[2]
     db_name: str = sys.argv[3]
-    state_name: str = sys.argv[4]
     host: str = "localhost"
     port: int = 3306
 
@@ -22,9 +21,14 @@ if __name__ == "__main__":
     )
     cursor = db.cursor()
 
+    # Execute the query
     cursor.execute(
-        "SELECT * FROM states WHERE name = %s ORDER BY id",
-        (state_name,)
+        """
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        ORDER BY cities.id
+        """
     )
 
     rows = cursor.fetchall()
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Lists all cities from the database hbtn_0e_4_usa
+Lists all cities of a state from the database hbtn_0e_4_usa
+using a state name provided as an argument
 """
 import sys
 import MySQLdb
@@ -9,13 +10,17 @@ if __name__ == "__main__":
     username: str = sys.argv[1]
     password: str = sys.argv[2]
     db_name: str = sys.argv[3]
+    arg: str = sys.argv[4]
     host: str = "localhost"
     port: int = 3306
 
     statement: str = """
-    SELECT *
-    FROM cities
-    ORDER BY id;
+    SELECT c.id, c.name, s.name
+    FROM cities AS c
+    JOIN states AS s
+    ON c.state_id = s.id
+    HAVING BINARY s.name = %s
+    ORDER BY c.id;
     """
 
     db = MySQLdb.connect(
@@ -27,7 +32,8 @@ if __name__ == "__main__":
     )
     cursor = db.cursor()
 
-    cursor.execute(statement)
+    cursor.execute(statement, (arg,))
     rows = cursor.fetchall()
-    for row in rows:
-        print(row)
+    cities = tuple(row[1] for row in rows)
+    cities = ', '.join(cities)
+    print(cities)
